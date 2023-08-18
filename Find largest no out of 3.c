9878eaefcde50a49676d0f8911347187f4af6efb
MONANK23 <140997714+MONANK23@users.noreply.github.com>
2023-08-18 11:11:44 +0530

    Add files via upload

---
+++ Find largest no out of 3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	printf("Enter The First Number  :");
+	scanf("%d",&a);
+	printf("Enter The Seacond Number  :");
+	scanf("%d",&b);
+	printf("Enter The Third Number  :");
+	scanf("%d",&c);
+	if(a>b && a>c)
+	{
+		printf("A is Largest Number");
+	}
+	else if(b>c)
+	{
+	printf("B is Largest Number");
+}
+else
+{
+	printf("C is Largest Number");
+}
+}
---
+++ Positive or negitive with else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	int n;
+	printf("Enter Number  :");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("No. Is Positive");
+		
+		}
+		else if(n<0)
+		{
+			printf("No. Is Negetive");
+			}	
+			else{
+				printf("no is zero");
+			}
+}
